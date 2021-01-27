5a440891a6a233526f20f15ef2993cc13b9e816f
mamewharu <mame0040007@gmail.com>
2021-01-27 18:00:07 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,8 @@ class PostsController < ApplicationController
  end
 
  def create
-  Post.create(content: params[:content])
-  redirect_to action: :index
+  post = Post.create(content: params[:content], checked: false)
+  render json:{ post: post }
  end
 
 #  既読を行ったときに実行されるアクション
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
   end
 
   item = Post.find(params[:id])
-  render json: { post: item }
+  render json:{ post: item }
  end
 
 end
